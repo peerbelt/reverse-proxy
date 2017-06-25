125c6c094df9b51fb7ccb410bbe48f5c12624a87
Peerbelt <build@peerbelt.com>
2017-06-25 15:02:12 -0400

    FEAT: Actual prod bindings.

--- app.js
+++ app.js
@@ -12,58 +12,50 @@ var fs = require( "fs" );
 // Create the HTTPS proxy server in front of a HTTP server
 //
 var binding = {
-    "yarn.pronto.peerbelt.com": {
-        target: "https://yarnpkg.com:443",
-        "changeOrigin": true
-    },
-    "google.pronto.peerbelt.com": {
-        target: "https://www.google.com:443",
-        "changeOrigin": true
-    },
-    "*.pronto.peerbelt.com": {
-        target: "https://www.microsoft.com:443/en-us/",
-        "changeOrigin": true
-    },
-    "pronto.peerbelt.com": {
+    "*.peerbelt.com": {
         target: {
-            host: "overlayads.herokuapp.com",
-            port: 443,
-            protocol: "https"
-        },
-        changeOrigin: true
+            host: "192.168.1.5",
+            port: 3000
+	}
+    },
+    "*.dyndns.info": {
+        target: {
+            host: "192.168.1.5",
+            port: 3001
+        }
     }
 };
 
 
 function socketErrorHandler( err, req ) {
     if ( req && ( req = req.socket ) ) {
-        try { 
+        try {
             req.end('HTTP/1.1 400 Bad Request\r\n\r\n');
         } catch( e ) {
             ;
-        } 
+        }
     }
 };
 
 httpProxy.createServer({
   binding: binding,
   target: {
-    host: "localhost",
-    port: 3030
+    host: "192.168.1.5",
+    port: 3000
   },
   ssl: {
     key: fs.readFileSync("../ssl-certificates/peerbelt.com/STAR_peerbelt_com-2017/peerbelt.com_pk.txt", "utf8"),
     cert: fs.readFileSync("../ssl-certificates/peerbelt.com/STAR_peerbelt_com-2017/peerbelt.com_crt.txt", "utf8")
   }
 })
-.listen(443)
+.listen(3443)
 .on( "error", socketErrorHandler );
 
 httpProxy.createServer({
   binding: binding,
   target: {
-    host: "localhost",
-    port: 3030
+    host: "192.168.1.5",
+    port: 3000
   }
-}).listen(80)
+}).listen(3080)
 .on( "error", socketErrorHandler );
